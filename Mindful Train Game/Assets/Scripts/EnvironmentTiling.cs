7309de2bb457e981f4fd8c3901d83c4ffae323e4
Alex <awoodycb@gmail.com>
2020-08-04 16:36:13 +1200

    Added train movement and basic environment tiling

---
+++ Mindful Train Game/Assets/Scripts/EnvironmentTiling.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentTiling : MonoBehaviour
+{
+    public GameObject environmentTile;
+
+    public float tileSize;
+
+    public GameObject endTile;
+    public GameObject lastTile;
+    public GameObject currentTile;
+    public GameObject nextTile;
+
+    public Vector3 lastTilePosition;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tileSize = environmentTile.GetComponent<MeshFilter>().mesh.bounds.size.z;
+        lastTile = Instantiate(environmentTile, new Vector3(0, 0, 0), Quaternion.identity);
+        lastTilePosition = lastTile.transform.position;
+        endTile = Instantiate(environmentTile, new Vector3(lastTilePosition.x, lastTilePosition.y, lastTilePosition.z - tileSize), Quaternion.identity);
+        currentTile = Instantiate(environmentTile, new Vector3(lastTilePosition.x, lastTilePosition.y, lastTilePosition.z + tileSize), Quaternion.identity);
+        nextTile = Instantiate(environmentTile, new Vector3(lastTilePosition.x, lastTilePosition.y, lastTilePosition.z + (2 * tileSize)), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(gameObject.transform.position, lastTile.transform.position) > Vector3.Distance(gameObject.transform.position, currentTile.transform.position))
+        {
+            Destroy(endTile);
+            endTile = lastTile;
+            lastTile = currentTile;
+            lastTilePosition = lastTile.transform.position;
+            currentTile = nextTile;
+            nextTile = Instantiate(environmentTile, new Vector3(lastTilePosition.x, lastTilePosition.y, lastTilePosition.z + (2 * tileSize)), Quaternion.identity);
+        }
+    }
+}
--- Mindful Train Game/Assets/Scripts/TrainMovement.cs
+++ Mindful Train Game/Assets/Scripts/TrainMovement.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class TrainMovement : MonoBehaviour
 {
+    public float trainSpeed = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(0f, 0f, trainSpeed * Time.deltaTime);
     }
 }
