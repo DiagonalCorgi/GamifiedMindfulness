4a5147d8f13122a98b0ed91df999d07196eb20fc
lgj19 <lgj19@uclive.ac.nz>
2020-08-10 15:11:00 +1200

    added clouds added cloud code
    Signed-off-by: lgj19 <lgj19@uclive.ac.nz>

---
+++ Mindful Train Game/Assets/Scripts/CloudCall.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ComplateProject
+{
+    public class CloudCall : MonoBehaviour
+    {
+        public ParticleSystem Cloud_Left;
+        public ParticleSystem Cloud_Right;
+        public bool test;
+
+        public void Update()
+        {
+            if (test == true)
+                {
+                Cloud_Call_Left();
+            }
+        }
+
+        public void Cloud_Call_Left()
+        {
+            Cloud_Left.emission.SetBursts(new[]
+            {
+                new ParticleSystem.Burst(0f, 10)
+            });
+
+
+
+            //GetComponent<ParticleSystem>().emission.SetBursts(new[]
+            //{
+            //                        new ParticleSystem.Burst(1f, 10), //float_time, short_count
+            //                    });
+        }
+}
+
+}
+
