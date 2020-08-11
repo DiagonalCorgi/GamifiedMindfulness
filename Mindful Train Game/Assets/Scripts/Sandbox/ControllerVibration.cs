da21468026b7240eb5eb11f2370df7f300363add
Angeline Moneda <fam65@uclive.ac.nz>
2020-08-11 17:28:07 +1200

    controller vibration implemented and theoretically working - just these controllers are annoying as heck

--- Mindful Train Game/Assets/Scripts/Sandbox/ControllerVibration.cs
+++ Mindful Train Game/Assets/Scripts/Sandbox/ControllerVibration.cs
@@ -8,29 +8,42 @@ public class ControllerVibration : MonoBehaviour
     public SteamVR_Action_Vibration hapticAction; //Vibration action
     public SteamVR_Action_Boolean trackpadAction; //note this is for vive Trigger to haptic when button when button pressed (testing)
 
-    // Start is called before the first frame update
-    // Use this later to call constant haptic
     void Start()
     {
+        InvokeRepeating("LeftHaptics", 2.0f, 2.0f);
+        //InvokeRepeating("RightHaptics", 2.0f, 2.0f);
+        Invoke("RepeatRightHaptics", 1.0f);
 
     }
 
     // Update is called once per frame
+    
+    
     void Update()
     {
-        // Left hand
-        if (trackpadAction.GetStateDown(SteamVR_Input_Sources.LeftHand))
-        {
-            Haptics(1, 150, 75, SteamVR_Input_Sources.LeftHand);
-        }
-
-        // Right Hand
-        if (trackpadAction.GetStateDown(SteamVR_Input_Sources.RightHand))
-        {
-            Haptics(1, 150, 75, SteamVR_Input_Sources.RightHand);
-        }
+        
+    }
+ 
+    //Haptics function for Left Hand
+    private void LeftHaptics()
+    {
+        Haptics(5, 170, 75, SteamVR_Input_Sources.LeftHand);
     }
 
+
+    // Haptics function for Right Hand
+    private void RightHaptics()
+    {
+        Haptics(5, 170, 75, SteamVR_Input_Sources.RightHand);
+    }
+
+    // To run in repeatedly after n seconds
+    private void RepeatRightHaptics()
+    {
+        InvokeRepeating("RightHaptics", 2.0f, 2.0f);
+    }
+
+    // The part that actually triggers the haptic to happen
     private void Haptics(float duration, float frequency, float amplitude, SteamVR_Input_Sources source)
     {
         hapticAction.Execute(0, duration, frequency, amplitude, source);
