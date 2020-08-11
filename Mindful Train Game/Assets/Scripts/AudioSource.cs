c044b3c67c9c48eb9fc642262235d3390f85c1cb
Cameron Urban <camblurban@gmail.com>
2020-08-12 01:20:37 +1200

    Updated FMOD Sounds and sound scripts

--- Mindful Train Game/Assets/Scripts/AudioSource.cs
+++ Mindful Train Game/Assets/Scripts/AudioSource.cs
@@ -6,19 +6,26 @@ using UnityEngine;
 public class AudioSource : MonoBehaviour
 {
     public EventInstance trainMetronome;
+    public EventInstance rhythm;
 
     [Range(0.0F, 2.0F)]
     public float audioSpeed = 1.5f;
 
+    public int rhythmIndex = 1;
+
     void Start()
     {
         trainMetronome = SoundM.CreateSoundInstance("TrainMetronome", transform, GetComponent<Rigidbody>());
+        rhythm = SoundM.CreateSoundInstance("Forest", transform, GetComponent<Rigidbody>());
 
         SoundM.PlaySound(trainMetronome);
+        SoundM.PlaySound(rhythm);
+        SoundM.SetSoundParameter("TrackVolume", 1);
     }
 
     void Update()
     {
         SoundM.SetSoundParameter("Speed", audioSpeed);
+        SoundM.SetSoundParameter("RhythmIndex", (float)rhythmIndex);
     }
 }
