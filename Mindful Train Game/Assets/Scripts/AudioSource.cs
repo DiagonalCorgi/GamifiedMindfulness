f61389399cc65354166340558bb6f41435f4fb51
cur11 <camblurban@gmail.com>
2020-08-13 16:55:26 +1200

    Synced vibrations to audio

--- Mindful Train Game/Assets/Scripts/AudioSource.cs
+++ Mindful Train Game/Assets/Scripts/AudioSource.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class AudioSource : MonoBehaviour
 {
+
     public EventInstance trainMetronome;
     public EventInstance rhythm;
 
@@ -12,6 +13,17 @@ public class AudioSource : MonoBehaviour
     public float audioSpeed = 1.5f;
 
     public int rhythmIndex = 1;
+
+    //Rhythms syncing
+    public int timelinePosition;
+    private int beatTimeout;
+
+    public float noteDelay = 0;
+
+    public List<string> leftHandRhythms;
+    public List<string> rightHandRhythms;
+    private int lhIndex = 0;
+    private int rhIndex = 0;
 
     void Start()
     {
@@ -27,5 +39,51 @@ public class AudioSource : MonoBehaviour
     {
         SoundM.SetSoundParameter("Speed", audioSpeed);
         SoundM.SetSoundParameter("RhythmIndex", (float)rhythmIndex);
+
+        int newTimelinePos;
+        rhythm.getTimelinePosition(out newTimelinePos);
+
+
+        beatTimeout -= (int)(Time.deltaTime * 1000);
+        beatTimeout = Mathf.Max(beatTimeout, 0);
+
+        float newTime = Mathf.Repeat((float)newTimelinePos / 1000, 4);
+
+
+        if (leftHandRhythms[rhythmIndex] != "" && rightHandRhythms[rhythmIndex] != "")
+        {
+            //parse list from string
+            string[] lhArray = leftHandRhythms[rhythmIndex].Split(',');
+            string[] rhArray = rightHandRhythms[rhythmIndex].Split(',');
+
+            float lhNextBeat = Mathf.Repeat(float.Parse(lhArray[lhIndex % lhArray.Length]), 4);
+            float rhNextBeat = Mathf.Repeat(float.Parse(rhArray[rhIndex % rhArray.Length]), 4);
+
+            //releasing beat puffs
+            if (beatTimeout == 0)
+            {
+                if (newTime >= lhNextBeat && newTime <= lhNextBeat + 0.1)
+                {
+                    beatTimeout = 100;
+
+                    //play release steam puff
+                    Debug.Log("play left");
+                    lhIndex++;
+                }
+
+                if (newTime >= rhNextBeat && newTime <= rhNextBeat + 0.1)
+                {
+                    beatTimeout = 100;
+
+                    //play release steam puff
+                    Debug.Log("play right");
+                    rhIndex++;
+                }
+            }
+        }
+
+        timelinePosition = newTimelinePos;
     }
+
+
 }
--- Mindful Train Game/Assets/Scripts/Sandbox/ControllerVibration.cs
+++ Mindful Train Game/Assets/Scripts/Sandbox/ControllerVibration.cs
@@ -8,11 +8,22 @@ public class ControllerVibration : MonoBehaviour
     public SteamVR_Action_Vibration hapticAction; //Vibration action
     public SteamVR_Action_Boolean trackpadAction; //note this is for vive Trigger to haptic when button when button pressed (testing)
 
+    public AudioSource audioManager;
+    private float vibrationTimeout = 0;
+    [Range(-0.5F, 0.5F)]
+    public float vibrationOffset = 0.15f;
+    [Range(0F, 7F)]
+    public float vibrationDuration = 1f;
+    [Range(0F, 250F)]
+    public float vibrationFrequency = 150f;
+    [Range(0F, 100F)]
+    public float vibrationAmplitude = 75f;
+
     void Start()
     {
-        InvokeRepeating("LeftHaptics", 2.0f, 2.0f);
+        //InvokeRepeating("LeftHaptics", 2.0f, 2.0f);
         //InvokeRepeating("RightHaptics", 2.0f, 2.0f);
-        Invoke("RepeatRightHaptics", 1.0f);
+        //Invoke("RepeatRightHaptics", 1.0f);
 
     }
 
@@ -21,27 +32,50 @@ public class ControllerVibration : MonoBehaviour
     
     void Update()
     {
-        
+        //activating controller vibration
+        vibrationTimeout -= (int)(Time.deltaTime * 1000);
+        vibrationTimeout = Mathf.Max(vibrationTimeout, 0);
+
+        float vibrationClock = Mathf.Repeat((float)audioManager.timelinePosition / 1000 + vibrationOffset, 2);
+
+        if (vibrationTimeout == 0)
+        {
+            if (vibrationClock >= 1 && vibrationClock <= 1.05)
+            {
+                vibrationTimeout = 100;
+
+                //play vibration
+                LeftHaptics();
+            }
+
+            if (vibrationClock >= 0 && vibrationClock <= 0.05)
+            {
+                vibrationTimeout = 100;
+
+                //play vibration
+                RightHaptics();
+            }
+        }
     }
  
     //Haptics function for Left Hand
     private void LeftHaptics()
     {
-        Haptics(5, 170, 75, SteamVR_Input_Sources.LeftHand);
+        Haptics(vibrationDuration, vibrationFrequency, vibrationAmplitude, SteamVR_Input_Sources.LeftHand);
     }
 
 
     // Haptics function for Right Hand
     private void RightHaptics()
     {
-        Haptics(5, 170, 75, SteamVR_Input_Sources.RightHand);
+        Haptics(vibrationDuration, vibrationFrequency, vibrationAmplitude, SteamVR_Input_Sources.RightHand);
     }
 
     // To run in repeatedly after n seconds
-    private void RepeatRightHaptics()
+    /*private void RepeatRightHaptics()
     {
         InvokeRepeating("RightHaptics", 2.0f, 2.0f);
-    }
+    }*/
 
     // The part that actually triggers the haptic to happen
     private void Haptics(float duration, float frequency, float amplitude, SteamVR_Input_Sources source)
